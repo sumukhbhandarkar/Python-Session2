0ec5090cb3c8009bdd2acc34396990b2bc619ec1
sumukhbhandarkar <sumukh.bhandarkar@oracle.com>
2022-03-18 11:50:54 +0530

    Modified test cases

--- test_calc.py
+++ test_calc.py
@@ -4,4 +4,4 @@ import calculator
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(calculator.add(1, 5), 6)
+        self.assertEqual(calculator.multiply(1, 4), 4)
